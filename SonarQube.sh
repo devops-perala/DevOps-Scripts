6a41c01f984faeaacf2121fcfee33c867dc742db
DevOps-Factory <factorydevops@gmail.com>
2025-06-19 13:33:26 +0530

    Update SonarQube.sh

--- SonarQube.sh
+++ SonarQube.sh
@@ -12,18 +12,49 @@ su - sonar
 #echo "user=admin & password=admin"
 
 ********************************************************************88
-
 #!/bin/bash
 
 set -e  # Exit on error
 
 echo "=== Step 1: Installing Java OpenJDK 11 ==="
-sudo amazon-linux-extras install java-openjdk11 -y
+
+# Detect OS and install Java
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    OS=$ID
+    VERSION_ID=$VERSION_ID
+else
+    echo "Unable to detect OS."
+    exit 1
+fi
+
+case "$OS" in
+  amzn)
+    if [[ "$VERSION_ID" == "2" ]]; then
+      sudo yum install -y amazon-linux-extras
+      sudo amazon-linux-extras enable java-openjdk11
+      sudo yum install -y java-11-openjdk
+    else
+      sudo dnf install -y java-11-amazon-corretto
+    fi
+    ;;
+  centos|rhel)
+    sudo yum install -y java-11-openjdk
+    ;;
+  ubuntu|debian)
+    sudo apt update
+    sudo apt install -y openjdk-11-jdk
+    ;;
+  *)
+    echo "Unsupported OS: $OS"
+    exit 1
+    ;;
+esac
 
 echo "=== Step 2: Downloading SonarQube ==="
 cd /opt/
 sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.6.50800.zip
-sudo unzip sonarqube-8.9.6.50800.zip
+sudo unzip -o sonarqube-8.9.6.50800.zip
 sudo rm -f sonarqube-8.9.6.50800.zip
 
 echo "=== Step 3: Creating 'sonar' user ==="
@@ -31,7 +62,6 @@ sudo useradd -r -s /bin/bash sonar || echo "User 'sonar' already exists"
 sudo chown -R sonar:sonar /opt/sonarqube-8.9.6.50800
 
 echo "=== Step 4: Setting permissions ==="
-# Safer than chmod 777
 sudo chmod -R 755 /opt/sonarqube-8.9.6.50800
 
 echo "=== Step 5: Starting SonarQube as 'sonar' user ==="
@@ -40,3 +70,4 @@ sudo -u sonar /opt/sonarqube-8.9.6.50800/bin/linux-x86-64/sonar.sh start
 echo "=== SonarQube Started Successfully ==="
 echo "Access SonarQube at: http://<your-server-ip>:9000"
 echo "Default credentials => username: admin, password: admin"
+
